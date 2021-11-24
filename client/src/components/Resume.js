ae99b30bdae6bd704668a91bf6c12879c0b59f0a
+ <+>
2021-11-24 13:44:08 -0800

    resume done

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,8 +1,17 @@
+import React from 'react'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
+import resume from './resume.pdf'
+
 function Resume() {
 
   return(
     <div className="resume">
       <h1>RESUME</h1>
+      <br />
+      <a href={resume} download>Click to Download</a>
+      <Document file={resume}>
+          <Page pageNumber={1} />
+      </Document>
     </div>
   )
 }
