fecfaa56599a62d5ea0455b4ec57c89097b68a15
+ <+>
2021-12-09 16:17:15 -0800

    added fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :about_cards, only: [:index]
   resources :projects, only: [:index]
   resources :blogs, only: [:index]
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
