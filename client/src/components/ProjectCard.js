c90f957f46bd224e24d4765b03a93467be8d41cb
Melissa Funk <melissafunk85@gmail.com>
2022-11-16 15:02:22 -0800

    removed Demo link if does not exit

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -7,12 +7,12 @@ function ProjectCard({ project }) {
         <div className="project-overlay">
           <h1 className="project-title" onClick={() => window.open(project.link, '_blank')}>{project.title}</h1>
           <h3 className="project-description">{project.description}</h3>
-          <h3 className="project-link" onClick={() => window.open(project.youtube, '_blank')}>Demo Video | </h3>
+          {project.youtube ? <h3 className="project-link" onClick={() => window.open(project.youtube, '_blank')}>Demo Video | </h3> : null}
           <h3 className="project-link" onClick={() => window.open(project.github, '_blank')}>Github</h3>
         </div>
       </div>
     </div>
   )
-}
+} 
 
 export default ProjectCard
